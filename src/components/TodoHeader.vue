2b315c6b603d7edad76fe149626444211e35cf19
yunseongho <sxdz1290@naver.com>
2023-08-17 22:43:16 +0900

    init

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,16 +1,18 @@
 <template>
   <header>
-    <h1>ToDo It!</h1>
+    <h1>TODO it!</h1>
   </header>
 </template>
+
 <script>
-export default {};
+export default {
+}
 </script>
 
-<style>
-h1 {
-  color: #2f3b52;
-  font-weight: 900;
-  margin: 2.5rem 0 1.5rem;
-}
-</style>   
+<style scoped>
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
-    <ul>
-      <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
+    <transition-group name="list" tag="ul">
+      <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
         <i class="checkBtn fas fa-check" aria-hidden="true"></i>
         {{ todoItem }}
         <span
@@ -12,36 +12,31 @@
           <i class="far fa-trash-alt" aria-hidden="true"></i>
         </span>
       </li>
-    </ul>
+      </transition-group>
   </section>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      todoItems: [],
-    };
-  },
-  created() {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const value = localStorage.getItem(key);
-        this.todoItems.push(value);
-      }
-    }
-  },
+  props: ["propsdata"],
   methods: {
     removeTodo(todoItem, index) {
-      localStorage.removeItem(todoItem);
-      this.todoItems.splice(index, 1);
+      this.$emit('removeTodo', todoItem, index);
     },
   },
 };
 </script>
 
 <style scoped>
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
 ul {
   list-style-type: none;
   padding-left: 0px;
